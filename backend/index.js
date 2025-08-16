c18f94719eba7992918719727e290109101a9081
wjkba <63542318+wjkba@users.noreply.github.com>
2025-08-16 11:05:13 +0200

    extract long reviews

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,66 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const url =
-  "https://letterboxd.com/film/corpus-christi-2019/reviews/by/activity/";
+const url = "https://letterboxd.com/film/parasite-2019/reviews/by/activity/";
 
-const reviews = {};
+let result = [];
 
-async function getHTML() {
+async function getHTML(url) {
   const { data: html } = await axios.get(url);
   return html;
 }
 
-async function main() {
-  const html = await getHTML();
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function extractReviewLinks(html) {
+  let reviewLinks = [];
+
   const $ = cheerio.load(html);
-  $(".production-viewing").each((i, review) => {
-    const reviewAuthorHref = $(review).find(".avatar").attr("href");
+  $(".production-viewing").each((i, reviewElement) => {
+    const reviewAuthorHref = $(reviewElement).find(".avatar").attr("href");
     const reviewAuthor = reviewAuthorHref.replace(/\//g, "");
     console.log("Author " + reviewAuthor);
 
-    const fullTextUrl = $(review)
+    const fullTextUrl = $(reviewElement)
       .find(".body-text:has(div.collapsed-text)")
       .attr("data-full-text-url");
     console.log(fullTextUrl);
 
-    console.log("====================");
+    if (fullTextUrl) {
+      const reviewLink = {
+        author: reviewAuthor,
+        fullTextUrl,
+      };
+      reviewLinks.push(reviewLink);
+    }
   });
+  return reviewLinks;
+}
+
+async function main() {
+  const html = await getHTML(url);
+  const reviews = extractReviewLinks(html);
+  console.log(reviews);
+
+  for (const review of reviews) {
+    const reviewHTML = await getHTML(
+      `https://letterboxd.com${review.fullTextUrl}`
+    );
+
+    const $ = cheerio.load(reviewHTML);
+    const text = $("p").text();
+    const newReview = {
+      author: review.author,
+      text,
+    };
+    result.push(newReview);
+
+    await delay(3000 + Math.random() * 2000);
+  }
+
+  console.log(result);
 }
 
 main().catch(console.error);
